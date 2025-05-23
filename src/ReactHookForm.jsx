8662b85b8325ae51e2eb8adeaf2c554abfb0105b
Imran Abbas <imranabbas67@gmail.com>
2025-05-23 11:18:55 -0700

    validation and error handleing

--- src/ReactHookForm.jsx
+++ src/ReactHookForm.jsx
@@ -3,17 +3,53 @@ import { useForm } from 'react-hook-form'
 
 export default function ReactHookForm() {
 
-  const { register, handleSubmit } = useForm();
+  const { register, handleSubmit, setError, formState: {errors, isSubmitting} } = useForm();
 
-  const onSubmit = (data) => {
-    console.log('FORM DATA = ', data)
+  const onSubmit = async (data) => {
+    try{
+      await new Promise(resolve => setTimeout(resolve, 1000));
+      // throw new Error()
+      console.log('FORM DATA = ', data)
+    }
+    catch(error){
+      setError('email', {
+        message: 'Email not found !'
+      })
+    }
   }
 
   return (
 		<form onSubmit={handleSubmit(onSubmit)}>
-			<input {...register('email')} type='email' placeholder='Email' />
-			<input {...register('password')} type='password' placeholder='Password' />
-      <button type='submit'>Submit</button>
+			<input
+				{...register('email', {
+					required: 'Email is required',
+					validate: (value) => {
+            if(!value.includes('@')){
+              return "Enter valid Email !"
+            }
+            return true
+          },
+				})}
+				type='text'
+				placeholder='Email'
+			/>
+			{errors.email && <div className='text-red-500'>{errors.email.message}</div>}
+
+			<input
+				{...register('password', {
+					required: 'Password is required',
+					minLength: {
+            value: 5,
+            message: 'Password must contain at least 5 characters !'
+          },
+				})}
+				type='password'
+				placeholder='Password'
+			/>
+			{errors.password && <div className='text-red-500'>{errors.password.message}</div>}
+			<button disabled={isSubmitting} type='submit'>
+        {isSubmitting ? 'Loading ...' : 'Submit'}
+      </button>
 		</form>
 	);
 }
